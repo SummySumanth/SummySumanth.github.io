f4697bafafff04c229b137ce5ab48644c0435233
SummySumanth <sumanth261094@gmail.com>
2023-07-26 23:59:26 +0530

    component creation

--- app/view/pages/App/App.jsx
+++ app/view/pages/App/App.jsx
@@ -31,7 +31,14 @@ function App() {
         <Link to="\">
           <BrandLogo styleName="welcome-text" />
         </Link>
-        <div styleName="navbar-container">
+        <div styleName="navbar-desktop">
+          <input type="checkbox" name="toggle-navbar" id="toggle-navbar" styleName="toggle-navbar" />
+          <label htmlFor="toggle-navbar" styleName="toggle-label">
+            <div styleName="barTop" />
+            <div styleName="barMiddle" />
+            <div styleName="barBottom" />
+          </label>
+
           <input type="radio" name="nav-tabs" styleName="link-bio" id="link-bio" />
           <input type="radio" name="nav-tabs" styleName="link-blogs" id="link-blogs" />
           <input type="radio" name="nav-tabs" styleName="link-projects" id="link-projects" />
@@ -39,7 +46,7 @@ function App() {
           <input type="radio" name="nav-tabs" styleName="link-techstack" id="link-techstack" />
           <input type="radio" name="nav-tabs" styleName="link-uses" id="link-uses" />
           <input type="radio" name="nav-tabs" styleName="link-contact" id="link-contact" />
-          <nav>
+          <nav styleName="navItems-container">
             <label htmlFor="link-bio" styleName="navItems navItem-bio">Bio</label>
             <label htmlFor="link-blogs" styleName="navItems navItem-blogs">Blogs</label>
             <label htmlFor="link-projects" styleName="navItems navItem-projects">Projects</label>
@@ -50,7 +57,6 @@ function App() {
             <div styleName="slider" />
           </nav>
         </div>
-
       </div>
     </motion.div>
   );
