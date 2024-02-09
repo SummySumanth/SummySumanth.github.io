8b7ac7d1b35bd87dfa604ef0760224a11421c6ab
SummySumanth <sumanth261094@gmail.com>
2024-02-09 20:54:34 +0530

    common theme switcher component

--- app/view/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ app/view/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
+import ThemeContext from '../../ThemeContext';
+import { sun, moon } from '../../images';
+import './themeSwitcher.css';
 
 function ThemeSwitcher() {
+  // using theme context to toggle theme
+  const toggleTheme = useContext(ThemeContext);
+
   return (
-    <>
-      <input id="theme-checkbox" styleName="theme-checkbox" type="checkbox" onChange={toggleTheme} />
-      <label htmlFor="theme-checkbox" styleName="theme-container">
-        <img styleName="theme-icons sun" alt="light mode" src={sun} />
-        <img styleName="theme-icons moon" src={moon} alt="dark mode" />
-      </label>
-    </>
+    <label htmlFor="theme-checkbox" styleName="theme-container">
+      <input id="theme-checkbox" styleName="theme-checkbox" type="checkbox" onChange={toggleTheme} />  
+      <img styleName="theme-icons sun" alt="light mode" src={sun} />
+      <img styleName="theme-icons moon" src={moon} alt="dark mode" />
+    </label>
   );
 }
 
--- app/view/pages/welcome/welcome.jsx
+++ app/view/pages/welcome/welcome.jsx
@@ -1,20 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 // import HamburgerBtn from '../../components/navbar/hamburgerBtn/HamburgerBtn';
 // import Links from '../../components/links/Links';
 import { motion } from 'framer-motion';
+import ThemeSwitcher from '../../components/ThemeSwitcher/ThemeSwitcher';
 import RoundedBtn from '../../components/RoundedBtn/RoundedBtn';
-import ThemeContext from '../../ThemeContext';
 import BrandLogo from '../../components/brandLogo/BrandLogo';
 
-import { fistBump, moon, sun } from '../../images/index';
+import { fistBump } from '../../images';
 
 import './welcome.css';
 
 function App() {
-  const toggleTheme = useContext(ThemeContext);
-
   const downloadResume = () => {
     window.location.assign(`${window.location.origin}/api/download/resume`);
   };
@@ -37,7 +35,6 @@ function App() {
 
         <div styleName="description-text">
           Ey yo! Thanks for showing interest in getting to know me !
-          {/* Ey yo! Thanks for hopping in, I have got some lit stuff to show you..  */}
 
           <Link to="/contact-us" styleName="description-text-enter">
             Get in
@@ -46,11 +43,8 @@ function App() {
           </Link>
         </div>
       </div>
-      <input id="theme-checkbox" styleName="theme-checkbox" type="checkbox" onChange={toggleTheme} />
-      <label htmlFor="theme-checkbox" styleName="theme-container">
-        <img styleName="theme-icons sun" alt="light mode" src={sun} />
-        <img styleName="theme-icons moon" src={moon} alt="dark mode" />
-      </label>
+      <ThemeSwitcher />
+
       <RoundedBtn styleName="downloadBtn" ctaText="Download Resume" cta={downloadResume} />
       {/* <Links
         links={links}
