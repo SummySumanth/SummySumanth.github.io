45fc060820ebe07c42d6f89314a9279e9b3b8d19
SummySumanth <sumanth261094@gmail.com>
2024-04-10 20:37:06 +0530

    mixpanel tracking integration

--- app/view/configs/configs.js
+++ app/view/configs/configs.js
@@ -1,3 +1,4 @@
 const API_URL = process.env.NODE_ENV === 'production' ? 'https://summy.dev' : 'http://localhost:8080';
+const ENV = process.env.NODE_ENV;
 
-export { API_URL };
+export { API_URL, ENV };
