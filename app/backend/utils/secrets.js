88cf829dfbdee616491d0a4dd6e4a66d4465bd06
SummySumanth <sumanth261094@gmail.com>
2024-04-17 12:30:36 +0530

    fix: :bug: Closing of navigation items screen on selection of nav item.

--- app/backend/utils/secrets.js
+++ app/backend/utils/secrets.js
@@ -12,13 +12,17 @@ async function accessSecret(keyName) {
 function Environment(NODE_ENV) {
   if (process.env.NODE_ENV === 'production') {
     const mediumToken = accessSecret('medium-api-token');
+    const hashnodeToken = accessSecret('hashnode-access-token');
     this.keysAndValues = {
       mediumToken,
+      hashnodeToken,
     };
   } else {
     const mediumToken = process.env.MEDIUM_API_KEY;
+    const hashnodeToken = process.env.HASNHNODE_API_KEY;
     this.keysAndValues = {
       mediumToken,
+      hashnodeToken,
     };
   }
   this.NODE_ENV = NODE_ENV || 'development';
